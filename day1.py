d70976fdf16bd7fcacf19d7f7c23ad5b8725c366
Aswathirepo <muraliaswathi211@gmail.com>
2025-09-19 12:10:42 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,14 @@
+def op(a,k,i):
+    if i<=k:
+        a=str(a)
+        b=a[::-1]
+        if a==b:
+            return [i,int(a)]
+        else:
+            a=int(a)+int(b)
+            i+=1
+            return op(a,k,i)
+        
+a=int(input())
+k=int(input())
+print(op(a,k,0))
